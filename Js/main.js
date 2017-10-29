79267b809712bc77cacb678bf7e1805fca960a85
Matt Zager <mzager09@gmail.com>
2017-10-29 19:20:59 -0400

    'nucommit'

--- Js/main.js
+++ Js/main.js
@@ -10,10 +10,14 @@ $('#numbers').html(q);
 }; 
 
 $('button').click(function(){
+//document.getElementById("Clearevery").disabled = true;
 v = ($(this).attr('value'));
 chain(v);
 });
 
-
+$('#Clearevery').click(function(){
+	q = " ";
+	$('#numbers').html(q);
+});
 
 });
