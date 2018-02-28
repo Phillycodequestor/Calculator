51e61506d9dbad2e7bd3f0e1400d9c76d8dc465d
Matt Zager <mzager09@gmail.com>
2018-02-28 11:28:49 -0500

    'nu'

--- Js/main.js
+++ Js/main.js
@@ -1,20 +1,14 @@
 $(document).ready(function(){
  
-var workingnumber = "";
 
-//i give up :(
+
+
 $('.number').click(function(){
-
-workingnumber += $(this).val();
-
-call();
 
 
 });
 
-function call(){
-	confirm(workingnumber);
-}
+
 });///ends docready
 
 
