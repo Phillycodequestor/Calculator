b73303b5c5b35bf1df796d14c86fc8388af3e765
Matt Zager <mzager09@gmail.com>
2018-03-02 11:11:12 -0500

    'nu'

--- Js/main.js
+++ Js/main.js
@@ -2,9 +2,19 @@ $(document).ready(function(){
  
 
 
+var setrunningtotal = (function(){
 
+var total = "";
+return function(num){
+	return total += num;
+};
+
+}());
+
+//Rig this to perform the operation
 $('.number').click(function(){
 
+alert(setrunningtotal(5));
 
 });
 
